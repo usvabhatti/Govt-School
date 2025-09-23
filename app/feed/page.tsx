8c9b2f6343a2122409492873ddfcc181fa2d67ae
Usva Iqbal Bhatti <fa23-bse-193@cuilahore.edu.pk>
2025-09-23 09:21:12 +0500

    fix the error

--- app/feed/page.tsx
+++
@@ -1,9 +0,0 @@
-// export default function Feed() {
-//     return(
-//          <div className="flex flex-col items-center mt-2">
-//         <img src="https://i.postimg.cc/Y9PZJMDW/image.png" alt="school logo" className="h-32 w-auto" />
-//         <p className="-mt-3 text-center text-sm font-semibold text-lg leading-tight text-[rgb(24,59,78)]">Government High School 01</p>
-
-//       </div>
-//     );
-// }
